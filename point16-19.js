8cc51300a809e2e96a2859bf78a2de5d34af185f
Deyola Fadwa Shifana <deyolafadwashifana@gmail.com>
2022-03-27 21:45:28 +0700

    2703

--- point16-19.js
+++ point16-19.js
@@ -7,18 +7,18 @@ pweb.get("/login", (req, res) => {
 
 pweb.listen(3000)
 
-pweb.get("/point16", function(req, res) {
+pweb.put("/point16", function(req, res) {
     res.send("Ubah komponen penilaian")
 })
 
-pweb.get("/point17", function(req, res) {
+pweb.delete("/point17", function(req, res) {
     res.send("Hapus komponen penilaian")
 })
 
-pweb.get("/point18", function(req, res) {
+pweb.post("/point18", function(req, res) {
     res.send("Tambah pertemuan mingguan RPS")
 })
 
-pweb.get("/point19", function(req, res) {
+pweb.put("/point19", function(req, res) {
     res.send("Ubah pertemuan mingguan yang ada dalam RPS")
 })
--- point6-10.js
+++ point6-10.js
@@ -7,22 +7,22 @@ pweb.listen (port,function(){
     console.log("materi 6-10")
 })
 
-pweb.get("/materi6", function(req,res){
+pweb.post("/materi6", function(req,res){
     res.send("Menambah RPS")
 })
 
-pweb.get("/materi7", function(req,res){
+pweb.put("/materi7", function(req,res){
     res.send("mengubah RPS ")
 })
 
-pweb.get("/materi8", function(req,res){
+pweb.put("/materi8", function(req,res){
     res.send("Merevisi RPS ")
 })
 
-pweb.get("/materi9", function(req,res){
+pweb.post("/materi9", function(req,res){
     res.send("Tambah CPMK Kuliah")
 })
 
-pweb.get("/materi10", function(req,res){
-    res.send("Hapus CPMK Kuliah")
+pweb.put("/materi10", function(req,res){
+    res.send("Ubah CPMK kuliah")
 })
