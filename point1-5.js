98c22c6814e258d75c40f11a5db48b585fbbd4d1
Deyola Fadwa Shifana <deyolafadwashifana@gmail.com>
2022-03-27 21:24:40 +0700

    abc

--- point1-5.js
+++ point1-5.js
@@ -6,20 +6,23 @@ point1_5.listen (port, function(){
     console.log("point1-5")
 })
 
-point1_5.get("/login", (req, res) => {
+point1_5.post("/signup", (req, res) => {
     res.send("Yeay Anda Berhasil Terhubung ke Server!!")
 })
 
+point1_5.get("/login", (req, res) => {
+    res.send("Yeay Anda Berhasil Terhubung ke Server!!")
+})
 
 point1_5.get("/point2", function(req, res) {
     res.send("Anda sudah Logout")
 })
 
-point1_5.get("/point3", function(req, res) {
+point1_5.put("/point3", function(req, res) {
     res.send("Tambah / Perbaharui RPS")
 })
 
-point1_5.get("/point4", function(req, res) {
+point1_5.post("/point4", function(req, res) {
     res.send("Admin dapat melihat laporan terkait RPS yang ad pertemuan mingguan yang ada dalam RPS")
 })
 
