4ce32b83e3d14336d0299537e1c7871a4a4fd27f
Deyola Fadwa Shifana <91823767+deyolaa@users.noreply.github.com>
2022-06-24 18:01:26 +0700

    hiii

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import express from "express";
 // Import cors
 import cors from "cors";
 // Import connection
-import db from "./config/database.js";
+import db from "./server/config/database.js";
 // Import router
-import Router from "./routes/routes.js";
+import Router from "./server/routes/routes.js";
  
 // Init express
 const app = express();
