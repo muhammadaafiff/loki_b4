ca959e108dbea4f87660cd667c7d616783c10daa
Deyola Fadwa Shifana <91823767+deyolaa@users.noreply.github.com>
2022-06-24 18:34:11 +0700

    connect aaaaa

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,9 @@
 import express from "express";
-import { getUser } from "../controllers/UserController.js";
+import { getUser } from "../controllers/User.js";
 
 const router = express.Router();
 
 router.get('/user', getUser)
+
+
+export default router;
