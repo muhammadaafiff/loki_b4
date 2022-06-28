8001babbf20ce413ee8bc9d650eea8db3f114e5e
Deyola Fadwa Shifana <91823767+deyolaa@users.noreply.github.com>
2022-06-29 03:43:30 +0700

    test

--- program/controllers/index.js
+++ program/controllers/index.js
@@ -1,9 +1,11 @@
 const test = require ("./test.js")
 const admin = require ("./admin.js")
+const auth = require('./auth.js')
 
 const controllers = {}
 
 controllers.test = test
 controllers.admin = admin
+controllers.auth = auth
 
-module.exports = controllers
+module.exports = controllers    
--- program/routes/admin.js
+++ program/routes/admin.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 const controllers = require('../controllers/index.js')
 const cekLogin = require('../middleware/cekLogin.js')
-const cekAdmin = require('../middleware/cekAdmin.js')
 
-server.get('/admin', (req, res) => {
+router.get('/admin', (req, res) => {
     res.render("loginAdmin")
 })
-server.post('/loginAdmin', controllers.auth.loginAdmin)
-server.get('/adminlistrps', cekLogin, controllers.admin.home)
+router.post('/loginAdmin', controllers.auth.loginAdmin)
+router.get('/adminlistrps', cekLogin, controllers.admin.home)
+
 
 module.exports = router
