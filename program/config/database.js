b14c8c75dfd878348923ad1d10e54c2b2346bb2f
ThomasAkram <91660351+ThomasAkram@users.noreply.github.com>
2022-07-02 02:05:39 +0700

    k

--- program/config/database.js
+++ program/config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('lokii', 'root', '', {
+const db = new Sequelize('loki', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
 })
